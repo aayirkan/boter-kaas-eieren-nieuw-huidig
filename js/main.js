9b0b60d7f6708952f008fab32944ac0da426e866
aayirkan <302210440@student.rocmondriaan.nl>
2023-01-12 15:34:18 +0100

    bijna af
    bijna klaar alleen alles op de goede plekzetten

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 console.log("Tic Tac Toe")
 
 const myBtn = document.querySelector('.resetName');
-myBtn.addEventListener('click', myFunction);
+// myBtn.addEventListener('click', myFunction);
+
+function resetBoard() {
+
+	// Reset tiles
+	for (let i = 0; i < playfieldArray.length; i++) {
+		let tile = fields[i];
+		tile.innerHTML = "";
+	}
+	currentPlayer = "X"; // Start with X
+	playfieldArray = [false, false, false, false, false, false, false, false, false];
+}
+
+const resetButton = document.querySelector(".resetBoard"); //A button to reset the game
+resetButton.addEventListener("click", resetBoard); // Add an event listener to the reset button
 
 const playerOneLabel = document.querySelector('.player1');
 const playerTwoLabel = document.querySelector('.player2');
 
-let player1Points = 0;
-let player2Points = 0;
+let player1Points = "X";
+let player2Points = "O";
 let playerOneName = "";
 let playerTwoName = "";
-let playerOnePlaying = true;
+let currentPlayer = "X";
+let gameEnded = false;
+
+let playerTurn = 1;
+
+let playfieldArray = [false, false, false, false, false, false, false, false, false];
+
 
 // let playerOne = prompt("Wat is de naam van speler 1?")
 // let playerTwo = prompt("Wat is de naam van speler 2?")
@@ -31,38 +51,81 @@ for (let i = 0; i < fields.length; i++) {
 	field.addEventListener('click', function () {
 		boxClicked(i);
 	});
+
+	checkWinner();
 }
-const winningConditions = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6],
-];
+
+function checkWinner(currentPlayer) {
+
+    let winningConditions = [
+		[0, 1, 2],// First horizontal row
+		[3, 4, 5],// Second horizontal row
+		[6, 7, 8],// Third horizontal row
+		[0, 3, 6],// First vertical row
+		[1, 4, 7],// Second vertical row
+		[2, 5, 8],// Third vertical row
+		[0, 4, 8],// First diagonal row
+		[2, 4, 6],// Second diagonal row
+]
+
+
+
+    for (let i = 0; i < winningConditions.length; i++) {
+		let matchCounter = 0;
+
+		
+		const firstSymbol = fields[winningConditions[i][0]].textContent;
+
+		const array = winningConditions[i];
+		//n is number
+		let nOne = array[0];
+		let nTwo = array[1];
+		let nThree = array[2];
+
+		if (playfieldArray[nOne] == currentPlayer && playfieldArray[nTwo] == currentPlayer && playfieldArray[nThree] == currentPlayer) {
+			alert(currentPlayer + " wint!");
+			gameEnded = true;
+
+        }
+	}
+}
+
+// const winningConditions = [
+// 	[0, 1, 2],
+// 	[3, 4, 5],
+// 	[6, 7, 8],
+// 	[0, 3, 6],
+// 	[1, 4, 7],
+// 	[2, 5, 8],
+// 	[0, 4, 8],
+// 	[2, 4, 6],
+// ];
+
 
 function boxClicked(index) {
-	// alert("Geklikt op vakje " + index);
+	console.log("Box clicked");
+
 	const field = fields[index];
-
-	// Alleen als het veldje leeg is, dus geen X of O
-
-	if (field.innerText != "") {
-
-		return;
-
-	}
-
-	if (playerOnePlaying) {
-		field.textContent = "X";
-		playerOnePlaying = false;
-	} else {
-		field.textContent = "O";
-		playerOnePlaying = true;
+	if (gameEnded == false) {
+		if (playerTurn == 1 && playfieldArray[index] == false) {
+			//speler 1(X)
+			currentPlayer = "X";
+			playerTurn = 2;
+			playfieldArray[index] = `X`;
+			field.textContent = currentPlayer;
+			checkWinner(`X`);
+		} else if (playerTurn == 2 && playfieldArray[index] == false) {
+			//speler 2 (O)
+			currentPlayer = "O";
+			playerTurn = 1;
+			playfieldArray[index] = `O`;
+			field.textContent = currentPlayer;
+			checkWinner(`O`);
+		}
+		console.log(playfieldArray)
 	}
 }
+
 
 
 
@@ -74,14 +137,40 @@ function boxClicked(index) {
 // playerTwoLabel.innerHTML = 'Player 2:' + playerTwoName + "<br>";
 // playerTwoLabel.innerHTML += 'Score:'+ player2Points;
 
-function myFunction() {
-	playerOneName = prompt(`Wat word de nieuwe naam van ${playerOne}?`);
-	playerOneLabel.innerHTML = 'Player 1:' + playerOneName + "<br>";
-	playerOneLabel.innerHTML += 'Score:' + player1Points;
+// function myFunction() {
+// 	playerOneName = prompt(`Wat word de nieuwe naam van ${playerOne}?`);
+// 	playerOneLabel.innerHTML = 'Player 1:' + playerOneName + "<br>";
+// 	playerOneLabel.innerHTML += 'Score:' + player1Points;
 
-	playerTwoName = prompt(`Wat word de nieuwe naam van ${playerTwo}?`)
-	playerTwoLabel.innerHTML = 'Player 2:' + playerTwoName + "<br>";
-	playerTwoLabel.innerHTML += 'Score:' + player2Points;
+// 	playerTwoName = prompt(`Wat word de nieuwe naam van ${playerTwo}?`)
+// 	playerTwoLabel.innerHTML = 'Player 2:' + playerTwoName + "<br>";
+// 	playerTwoLabel.innerHTML += 'Score:' + player2Points;
+// }
+
+function changePlayer() {
+
+    currentPlayer = "X"
+
 }
 
+
+
+
+function myFunction() {
+
+    playerOneName = prompt(' in wat wil je Speler 1 naam veranderen');
+
+    playerOneLabel.innerHTML = 'Player 1:' + playerOneName + "<br>";
+
+    playerOneLabel.innerHTML += 'Symbol:' + player1symbol;
+
+
+
+    playerTwoName = prompt('in wat wil je Speler 2 naam veranderen');
+
+    playerTwoLabel.innerHTML = 'Player 2:' + playerTwoName + "<br>";
+
+    playerTwoLabel.innerHTML += 'Symbol:' + player2symbol;
+
+}
 
